5c55d669eac153660c461edf8f2fb3c33b85a2d4
Kenta Urano <vrano@ucl.nuee.nagoya-u.ac.jp>
2019-02-06 18:11:05 +0900

    minimal vue.js test app runs

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// from https://tisnote.com/vue-webpack-yarn/
+
+const path = require('path');
+ 
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'www',
+    port: 4000,
+    inline: true
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // おまじない
+    }
+  }
+};
