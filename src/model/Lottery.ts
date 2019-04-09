a4b487e354287c2ba1f2b6e14d57f2c2c3435231
iida <iida@ucl.nuee.nagoya-u.ac.jp>
2019-04-09 16:17:44 +0900

    implement weighted Lottery

--- src/model/Lottery.ts
+++ src/model/Lottery.ts
@@ -82,11 +82,19 @@ export class Lottery {
         const winnerId = Math.floor(Math.random() * this.candidates.length)
         const winner = this.candidates[winnerId]
 
-        this.candidates.forEach((m, i) => m[Lottery.WINNER] = (i === winnerId))
+        this.setWinner(winnerId)
       } else {
         console.log('No member selected!!')
       }
     }
+  }
+
+  protected setWinner (idx: number) {
+    this.candidates.forEach((m, i) => m[Lottery.WINNER] = (i === idx))
+  }
+
+  protected setWinners (idxs: Array<number>) {
+    this.candidates.forEach((m, i) => m[Lottery.WINNER] = (idxs.includes(i)))
   }
 }
 
@@ -95,7 +103,9 @@ export class WeightedLottery extends Lottery {
   static WEIGHT = 'weight'
   static RANK = 'rank'
 
-  constructor (candidates?: Array<any>, status?: Status) {
+  private _winnerNum: number
+
+  constructor (candidates?: Array<any>, status?: Status, winnerNum?: number) {
     super(candidates, status)
 
     this.candidates.forEach(m => {
@@ -106,18 +116,59 @@ export class WeightedLottery extends Lottery {
         console.log('please set rank')
       }
     })
+
+    this._winnerNum = winnerNum ? winnerNum : 1
+  }
+
+  private get winnerNum (): number {
+    return this._winnerNum
   }
 
   public run (): void {
-    this.setRunningState()
-    if (this.candidates.length !== 0) {
-      const winnerId = Math.floor(Math.random() * this.candidates.length)
-      const winner = this.candidates[winnerId]
+    if (this.isRunning()) {
+      if (this.hasCandidate()) {
+        let order = []
 
-      winner.winner = true
-    } else {
-      console.log('No member selected!!')
+        this.candidates.forEach((m, i) => {
+          m[WeightedLottery.RANK] = Math.random() * m[WeightedLottery.WEIGHT]
+          order.push([m[WeightedLottery.RANK], i])
+        })
+
+      } else {
+        console.log('No member selected!!')
+      }
     }
-    this.setFinishState()
+  }
+
+  protected setWinner (arg: number | Array<any>) {
+    if (typeof arg === 'number') {
+      super.setWinner(arg)
+    } else {
+      arg.sort((a, b) => a[0] - b[0])
+      console.log(arg)
+      let winners = []
+      for (let i = 0; i < this.winnerNum; i++) {
+        winners.push(i)
+      }
+      super.setWinners(winners)
+    }
+  }
+
+  private hasCandidate () {
+    return !this.candidates.every(m => m.weight === 0)
+  }
+
+  private normalize () {
+    let weightSum = 0
+    this.candidates.forEach(m => weightSum += m[WeightedLottery.WEIGHT])
+    if (weightSum !== 0) {
+      this.candidates.forEach(m => m[WeightedLottery.WEIGHT] /= weightSum)
+    }
+
+    let rankSum = 0
+    this.candidates.forEach(m => rankSum += m[WeightedLottery.RANK])
+    if (rankSum !== 0) {
+      this.candidates.forEach(m => m[WeightedLottery.RANK] /= rankSum)
+    }
   }
 }
